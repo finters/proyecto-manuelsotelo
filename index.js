effc8b40d5f1bb3068cc386cd9e10955f612a1f9
Jose Roberto Quevedo Gabizon <jose.quevedo2011@gmail.com>
2024-02-18 14:40:06 -0400

    feat: :sparkles: api 2324-2

--- index.js
+++ index.js
@@ -1,107 +1,111 @@
 const fs = require('fs');
+const dayjs  = require('dayjs');
 const { fakerES: faker } = require('@faker-js/faker');
 
 
-let objects = new Array(100).fill({}).map((_) => {
-  const type = faker.helpers.arrayElement(['student', 'professor'])
+let users = new Array(100).fill({}).map((_) => {
+  const type = faker.helpers.arrayElement(['listener', 'musician']);
   const firstName = faker.person.firstName();
   const lastName = faker.person.lastName();
   const obj = {
     id: faker.string.uuid(),
-    firstName,
-    lastName,
+    name: `${firstName} ${type === 'listener' ? lastName : ''}`,
     email: faker.internet.email({
-      firstName,
-      lastName,
+      firstName: firstName.toLowerCase(),
+      lastName: lastName.toLowerCase(),
       allowSpecialCharacters: false,
-      provider: type === 'student' ? 'correo.unimet.edu.ve' : 'unimet.edu.ve',
+      provider: 'unimet.edu.ve',
     }),
     username: faker.internet.userName(),
     type,
   };
-  if (type === 'student') {
-    obj.major = faker.helpers.arrayElement([
-      'Ingeniería Civil',
-      'Ingeniería Eléctrica',
-      'Ingeniería Mecánica',
-      'Ingeniería de Producción',
-      'Ingeniería Química',
-      'Ingeniería de Sistemas',
-      'Contaduría Publica',
-      'Economía Empresarial',
-      'Ciencias Administrativas',
-      'Psicología',
-      'Matematicas Industriales',
-      'Idiomar Modernos',
-      'Educación',
-      'Derecho',
-      'Estudios Liberales'
-    ]);
-  } else {
-    obj.department = faker.helpers.arrayElement([
-      'Banca, Contabilidad y Auditoría',
-      'Economía',
-      'Gerencia y Emprendimiento',
-      'Finanzas',
-      'Mercadeo',
-      'Ciencias de la Educación',
-      'Ciencias del Comportamiento',
-      'Desarrollo Integral',
-      'Humanidades',
-      'Física',
-      'Inglés',
-      'Lingüística',
-      'Matemáticas',
-      'Química',
-      'Estudios Internacionales',
-      'Estudios Jurídicos',
-      'Estudios Políticos',
-      'la Construcción y Desarrollo Sustentable',
-      'Energía y Automatización',
-      'Producción Industrial',
-      'Gestión de Proyectos y Sistemas',
-    ]);
-  }
+  // if (type === 'listener') {
+  //   obj.major = faker.helpers.arrayElement([
+  //     'Ingeniería Civil',
+  //     'Ingeniería Eléctrica',
+  //     'Ingeniería Mecánica',
+  //     'Ingeniería de Producción',
+  //     'Ingeniería Química',
+  //     'Ingeniería de Sistemas',
+  //     'Contaduría Publica',
+  //     'Economía Empresarial',
+  //     'Ciencias Administrativas',
+  //     'Psicología',
+  //     'Matematicas Industriales',
+  //     'Idiomar Modernos',
+  //     'Educación',
+  //     'Derecho',
+  //     'Estudios Liberales',
+  //   ]);
+  // } else {
+  //   obj.department = faker.helpers.arrayElement([
+  //     'Banca, Contabilidad y Auditoría',
+  //     'Economía',
+  //     'Gerencia y Emprendimiento',
+  //     'Finanzas',
+  //     'Mercadeo',
+  //     'Ciencias de la Educación',
+  //     'Ciencias del Comportamiento',
+  //     'Desarrollo Integral',
+  //     'Humanidades',
+  //     'Física',
+  //     'Inglés',
+  //     'Lingüística',
+  //     'Matemáticas',
+  //     'Química',
+  //     'Estudios Internacionales',
+  //     'Estudios Jurídicos',
+  //     'Estudios Políticos',
+  //     'la Construcción y Desarrollo Sustentable',
+  //     'Energía y Automatización',
+  //     'Producción Industrial',
+  //     'Gestión de Proyectos y Sistemas',
+  //   ]);
+  // }
   return obj;
 });
 
-const ids = objects.map((obj) => obj.id);
 
-objects = objects.map((obj) => {
-  obj.following = faker.helpers.arrayElements(ids, {min: 3, max: 15}).filter((id) => id !== obj.id);
-  return obj;
-})
+const musicians = users.filter((obj) => obj.type ==='musician');
+const listeners = users.filter((obj) => obj.type === 'listener');
 
-fs.writeFileSync('./users.json', JSON.stringify(objects, null, 2));
-
-const posts = new Array(1000).fill({}).map((_) => {
-  const type = faker.helpers.arrayElement(['photo', 'video']);
+const albums = new Array(300).fill({}).map((_) => {
+  const name = faker.word.words(3);
   const obj = {
-    publisher: faker.helpers.arrayElement(ids),
-    type,
-    caption: faker.word.words({ count: { min: 10, max: 75 } }),
-    date: faker.date.past(),
-    tags: faker.word.words({ count: { min: 1, max: 5 } }).split(' '),
+    id: faker.string.uuid(),
+    name,
+    description: faker.lorem.paragraphs(2),
+    cover: faker.image.urlPlaceholder({
+      height: 800,
+      width: 800,
+      text: name,
+      format: 'jpg'
+    }),
+    published: faker.date.past().toISOString(),
+    genre: faker.music.genre(),
+    artist: faker.helpers.arrayElement(musicians).id,
+    tracklist: new Array(faker.number.int({ min: 1, max: 12 })).fill({}).map((_) => ({
+      id: faker.string.uuid(),
+      name: faker.word.words(3),
+      duration: dayjs(new Date(2024, 1, 15, 0, faker.number.int({min: 1, max: 5}), faker.number.int({min: 0, max: 59}), 0)).format('mm:ss')
+    }))
   };
-  if (type === 'photo') {
-    obj.multimedia = {
-      type,
-      url: faker.image.urlPicsumPhotos(),
-    };
-  } else {
-    obj.multimedia = {
-      type,
-      url: faker.helpers.arrayElement([
-        'https://youtu.be/0bnSwzT7CX8?si=PriJ2uAVYwMgrH_6',
-        'https://youtu.be/s07kXl58qWM?si=iJcS8F4FAM7psZZO',
-        'https://youtu.be/40UQGZ1jgA4?si=XERwuMDaKWyGiFQZ',
-        'https://youtu.be/e1JbzSvuIdY?si=JOq3tmylEPuPUwm6',
-        'https://youtu.be/04mmSIDwmeg?si=IXKfHyIaioqPA8_-',
-        'https://youtu.be/WN-92Cfrk4o?si=hQBY5i1Qs2mkS-ct',
-      ]),
-    };
-  }
   return obj;
 });
 
-fs.writeFileSync('./posts.json', JSON.stringify(posts, null, 2));
+const songs = albums.flatMap((obj) => obj.tracklist);
+
+const playlists = new Array(100).fill({}).map((_) => {
+  const obj = {
+    id: faker.string.uuid(),
+    name: faker.word.words(3),
+    description: faker.lorem.paragraphs(2),
+    creator: faker.helpers.arrayElement(listeners).id,
+    tracks: new Array(faker.number.int({ min: 10, max: 100 })).fill({}).map((_) => faker.helpers.arrayElement(songs).id),
+  };
+  return obj;
+});
+
+fs.writeFileSync('./users.json', JSON.stringify(users, null, 2));
+fs.writeFileSync('./albums.json', JSON.stringify(albums, null, 2));
+fs.writeFileSync('./playlists.json', JSON.stringify(playlists, null, 2));
